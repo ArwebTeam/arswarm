f1b493bde0c829cf5df9a0f0b6bc8ac1731def13
Maciej Krüger <mkg20001@gmail.com>
2019-10-19 19:26:40 +0200

    feat: add arql local executor

--- src/arqlLocalExecutor.js
+++ src/arqlLocalExecutor.js
@@ -1,5 +1,40 @@
 'use strict'
 
-module.exports = (localTransactionCloud, query) => {
+module.exports = (cache) => {
+  const fncs = {
+    and: async (expr1, expr2) => {
+      const t1 = await interpreter(expr1)
+      const t2 = await interpreter(expr2)
+
+      return t1.filter(t => t2.indexOf(t) !== -1)
+    },
+    or: async (expr1, expr2) => {
+      const t1 = await interpreter(expr1)
+      const t2 = await interpreter(expr2)
+
+      t2.forEach(t => {
+        if (t1.indexOf(t) === -1) {
+          t1.push(t)
+        }
+      })
+
+      return t1
+    },
+    equals: async (expr1, expr2) => { // TODO: convert params to encoded format
+      return cache.getKVTX(expr1, expr2)
+    }
+  }
+
+  async function interpreter (q) {
+    const {op, expr1, expr2} = q
+
+    const f = fncs[op]
+    return f(expr1, expr2)
+  }
+
   // TODO: get transactions matching query from localTransactionCloud
+
+  return async (query) => {
+    return interpreter(query)
+  }
 }
--- src/cache.js
+++ src/cache.js
@@ -3,6 +3,8 @@
 const Transaction = require('arweave/web/lib/transaction').default
 const DB = require('./storage')
 
+const ARQL = require('./arqlLocalExecutor')
+
 module.exports = async ({ }) => {
   const txCache = new Set()
   const db = await DB()
@@ -19,6 +21,7 @@ module.exports = async ({ }) => {
 
       const tx = new Transaction(data)
 
+      // TODO: rework to use secondary index
       await Promise.all(tx.get('tags').map(async (tag) => {
         let key = tag.get('name', {decode: false, string: true})
         let value = tag.get('value', {decode: false, string: true})
@@ -64,9 +67,15 @@ module.exports = async ({ }) => {
 
       throw new Error('TX not found')
     },
+    getKVTags: async (kv) => {
+      // TODO: rework to use secondary index
+      return await db.get('kvTags', kv)
+    },
     batchAdd: async (txs) => {
       await Promise.all(txs.map(C.add))
     },
     db
   }
+
+  C.arql = ARQL(C)
 }
