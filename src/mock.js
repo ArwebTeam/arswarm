0f1ae7b00610834ece608863500b98edc5102160
Maciej Krüger <mkg20001@gmail.com>
2019-10-19 19:41:50 +0200

    fix: mock

--- src/mock.js
+++ src/mock.js
@@ -1,12 +1,16 @@
 'use strict'
 
-module.exports = ({storage}) => {
+const Cache = require('./cache')
+
+module.exports = async (config) => {
+  const cache = await Cache(config.cache)
+
   return {
     arql: async (query) => {
-
+      return cache.arql(query)
     },
     publish: async (tx) => {
-
+      return cache.add(tx)
     }
   }
 }
