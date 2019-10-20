62b154e32d5382d64d4e417a943b993d2f490ac8
Maciej Krüger <mkg20001@gmail.com>
2019-10-20 16:40:42 +0200

    feat: initial pubsub stuff

--- src/swarm.js
+++ src/swarm.js
@@ -2,16 +2,28 @@
 
 const Node = require('./node')
 
-const { verifyTX } = require('./tx')
+const { decodeTX, verifyTX } = require('./tx')
 
 module.exports = async (conf, cache) => {
   const node = await Node(conf, cache)
+
+  await prom(cb => node.pubsub.subscribe(
+    'arswarm', // TODO: possibly split up networks by app-id
+    async (msg) => {
+      const txData = await decodeTX(msg.data)
+      await verifyTX(txData)
+      await cache.add(txData)
+    }
+  ))
 
   return {
     searchForNewTransactions: async (query) => {
 
     },
     publishTransaction: async (query) => {
+
+    },
+    fetch: async (id) => {
 
     }
   }
